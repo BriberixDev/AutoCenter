779c90f4c17ef0d3fc3c3306a592875a92f03d02
Briberix <briberix.dev@gmail.com>
2025-08-19 00:34:46 +0200

    Add AutoCenterDbContext and configure EF Core with SQLite

---
+++ AutoCenter/Infrastructure/Data/AutoCenterDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AutoCenter.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoCenter.Web.Infrastructure.Data
+{
+    public class AutoCenterDbContext:DbContext
+    {
+        public AutoCenterDbContext(DbContextOptions<AutoCenterDbContext> options)
+            : base(options)
+        { }
+        public DbSet<User> Users { get; set; }= null!;
+
+        // Define DbSet properties for your entities here
+        // public DbSet<User> Users { get; set; }
+        // public DbSet<Vehicle> Vehicles { get; set; }
+        // Add other DbSet properties as needed
+        // You can also override OnConfiguring and OnModelCreating methods if necessary
+    }
+}
---
+++ AutoCenter/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoCenter.Web.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+
+        // Additional properties can be added as needed
+    }
+}

43a77d54724adbce7e3d737e6d9c60a1e193104a
Briberix <briberix.dev@gmail.com>
2025-08-19 00:34:46 +0200

    Add AutoCenterDbContext and configure EF Core with SQLite

--- AutoCenter/Program.cs
+++ AutoCenter/Program.cs
@@ -1,6 +1,12 @@
+using AutoCenter.Web.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AutoCenterDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();

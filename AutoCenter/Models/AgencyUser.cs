8a3ff0d932a74c8857b15a864f3223506b7b7a40
Briberix <briberix.dev@gmail.com>
2025-08-19 13:25:56 +0200

    Add basic models: User, Vehicle, listing, AgencyUser

---
+++ AutoCenter/Models/AgencyUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoCenter.Web.Models
+{
+    public class AgencyUser:User
+    {
+        public required string AgencyName{ get; set; } = string.Empty;
+        public required string AgencyAddress { get; set; } = string.Empty;
+        public required string CompanyId { get; set; } = string.Empty;
+        [Url]
+        public string WebsiteUrl { get; set; } = string.Empty;
+    }
+}

b8a6a3bd7fe6146381c75845dc116cd8147ee568
Briberix <briberix.dev@gmail.com>
2025-08-19 17:13:01 +0200

    feat(models): add enums for TransmissionType, FuelType, BodyType, VehicleColor

---
+++ AutoCenter/Enums/BodyType.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoCenter.Web.Enums
+{
+    public enum BodyType
+    {
+        [Display(Name = "Sedan")]
+        Sedan,
+
+        [Display(Name = "Hatchback")]
+        Hatchback,
+
+        [Display(Name = "Wagon")]
+        Wagon,
+
+        [Display(Name = "Coupe")]
+        Coupe,
+
+        [Display(Name = "SUV")]
+        SUV
+    }
+}

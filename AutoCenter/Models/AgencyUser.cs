0837ec5cc2427f5530ef2a17917d09e6273a40f1
Briberix <briberix.dev@gmail.com>
2025-08-19 22:16:42 +0200

    refactor(models): remove C# required keyword from entities

--- AutoCenter/Models/AgencyUser.cs
+++ AutoCenter/Models/AgencyUser.cs
@@ -4,9 +4,9 @@ namespace AutoCenter.Web.Models
 {
     public class AgencyUser:User
     {
-        public required string AgencyName{ get; set; } = string.Empty;
-        public required string AgencyAddress { get; set; } = string.Empty;
-        public required string CompanyId { get; set; } = string.Empty;
+        public  string AgencyName{ get; set; } = string.Empty;
+        public  string AgencyAddress { get; set; } = string.Empty;
+        public  string CompanyId { get; set; } = string.Empty;
         [Url]
         public string WebsiteUrl { get; set; } = string.Empty;
     }
--- AutoCenter/Models/User.cs
+++ AutoCenter/Models/User.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         
-        public required string Name { get; set; }
-        public required string Email { get; set; }
+        public  string Name { get; set; }
+        public  string Email { get; set; }
         public string? PhoneNumber { get; set; }
         public bool Role { get; set; }=false;
         public DateTime RegistrationDate { get; private set; } = DateTime.Now;

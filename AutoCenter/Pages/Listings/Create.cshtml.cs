1b63eda6f652cc1f643221b6be4fd7e650870465
Briberix <briberix.dev@gmail.com>
2025-08-19 22:17:35 +0200

    feat(listing): scaffold CRUD pages

---
+++ AutoCenter/Pages/Listings/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AutoCenter.Web.Infrastructure.Data;
+using AutoCenter.Web.Models;
+
+namespace AutoCenter.Web.Pages.Listings
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AutoCenter.Web.Infrastructure.Data.AutoCenterDbContext _context;
+
+        public CreateModel(AutoCenter.Web.Infrastructure.Data.AutoCenterDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Listing Listing { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Listings.Add(Listing);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ AutoCenter/Pages/Listings/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AutoCenter.Web.Infrastructure.Data;
+using AutoCenter.Web.Models;
+
+namespace AutoCenter.Web.Pages.Listings
+{
+    public class EditModel : PageModel
+    {
+        private readonly AutoCenter.Web.Infrastructure.Data.AutoCenterDbContext _context;
+
+        public EditModel(AutoCenter.Web.Infrastructure.Data.AutoCenterDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Listing Listing { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing =  await _context.Listings.FirstOrDefaultAsync(m => m.Id == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+            Listing = listing;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Listing).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ListingExists(Listing.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ListingExists(int id)
+        {
+            return _context.Listings.Any(e => e.Id == id);
+        }
+    }
+}

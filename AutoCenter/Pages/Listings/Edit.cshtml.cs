101700dda10440549b9caeb68b510e5c5481943d
Briberix <briberix.dev@gmail.com>
2025-08-22 16:56:01 +0200

    feat(listing): implement Edit and Delete flow
    - Add Edit page and handlers
    - Validate inputs on server
    - Add Delete page
    - Add _ValidationScriptsPartial for DRY Edit and Create

--- AutoCenter/Pages/Listings/Edit.cshtml.cs
+++ AutoCenter/Pages/Listings/Edit.cshtml.cs
@@ -19,6 +19,30 @@ namespace AutoCenter.Web.Pages.Listings
         {
             _context = context;
         }
+        [BindProperty]
+        public Listing Listing { get; set; } = new Listing();
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            try
+            {
+                _context.Listings.Add(Listing);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            catch (DbUpdateException ex)
+            {
+                ModelState.AddModelError(string.Empty, "Failed to save. Try again.");
+                return Page();
+            }
+        }
 
     }
 }

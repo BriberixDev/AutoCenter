1223c45243de37b91897afc3ff3eba6d2ec841bc
Briberix <briberix.dev@gmail.com>
2025-08-26 20:35:18 +0200

    feat(auth): add cookie authentication scheme and login page

--- AutoCenter/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AutoCenter/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,28 +1,50 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace AutoCenter.Web.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
         [BindProperty]
-        public Credential Credential { get; set; } = new Credential();
+        public InputModel Input { get; set; }
         public void OnGet()
         {
         }
-        public void OnPost()
+        public async Task<IActionResult> OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            //Verify the credential
+            if (Input.UserName == "admin" && Input.Password == "Password")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email,"admin@mywebsite.com")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
 
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
         }
-    }
-    public class Credential
-    {
-        [Required]
-        [Display(Name = "User Name")]
-        public string UserName { get; set; } = string.Empty;
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; } = string.Empty;
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "User Name")]
+            public string UserName { get; set; } = string.Empty;
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; } = string.Empty;
+        }
     }
 }

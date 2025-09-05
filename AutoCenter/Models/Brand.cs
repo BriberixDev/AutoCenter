16a08c4b46e549456e29995fe719f3272c3607ce
Briberix <briberix.dev@gmail.com>
2025-09-05 20:46:03 +0200

    chore(models): Add Brand and Search models

---
+++ AutoCenter/Models/Brand.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoCenter.Web.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<VehicleSpecs> Vehicles { get; set; } = new List<VehicleSpecs>();
+    }
+}
---
+++ AutoCenter/Models/SearchParams.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoCenter.Web.Models
+{
+    public class SearchParams
+    {
+        public int? BrandId { get; set; }
+        public int? ModelId { get; set; }
+
+        [Range(0, 10_000_000, ErrorMessage = "PriceFrom must be between 0 and 10,000,000")]
+        public decimal? PriceFrom { get; set; }
+        [Range(0, 10_000_000, ErrorMessage = "PriceTo must be between 0 and 10,000,000")]
+        public decimal? PriceTo { get; set; }
+
+        [Range(1900, 2025, ErrorMessage = "First registration must be between 1900 and 2025")]
+        public int? YearFrom { get; set; }
+        [Range(1900, 2025, ErrorMessage = "First registration must be between 1900 and 2025")]
+        public int? YearTo { get; set; }
+
+
+    }
+}

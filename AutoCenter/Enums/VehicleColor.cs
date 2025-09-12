0f74f3356811b18e38cfb4890b5f67d4b6c6faca
Briberix <briberix.dev@gmail.com>
2025-09-12 22:50:22 +0200

    refactor(enums): rename VehicleSpecsColor to VehicleSpecColor

--- AutoCenter/Enums/VehicleColor.cs
+++ AutoCenter/Enums/VehicleColor.cs
@@ -1,6 +1,6 @@
 ﻿namespace AutoCenter.Web.Enums
 {
-    public enum VehicleSpecsColor
+    public enum VehicleSpecColor
     {
         White=1, Black=2, Gray=3, Blue=4, Red=5, Green=6, Yellow=7, Brown=8, Silver=9
     }

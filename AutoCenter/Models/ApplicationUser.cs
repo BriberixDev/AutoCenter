a6950010e1b7ca7ff0036dd31f62e3f48f8e6dbb
Briberix <briberix.dev@gmail.com>
2025-09-15 20:36:52 +0200

    feat(identity): replace User model with ApplicationUser : IdentityUser
    - Introduce ApplicationUser : IdentityUser with Name, RegistrationDate, Listings
    - Make AgencyUser extend by ApplicationUser.

---
+++ AutoCenter/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+    namespace AutoCenter.Web.Models
+    {
+        public class ApplicationUser: IdentityUser
+        {
+            //public int Id { get; set; }
+
+            //[Required, MaxLength(40)]
+            public required string Name { get; set; }
+
+            //[Required,EmailAddress, MaxLength(50)]
+            //public  required string Email { get; set; }
+
+            //public string? PhoneNumber { get; set; }
+
+            //public bool Role { get; set; }=false;
+
+            public DateTime RegistrationDate { get; private set; } = DateTime.Now;
+
+            public List<Listing> Listings { get; set; } = new ();
+        }
+    }

ad8d6abaad2276dd996f521a0623b67ae39a8b57
Briberix <briberix.dev@gmail.com>
2025-09-15 20:39:56 +0200

    refactor(models): rename VehicleId→VehicleSpecId

--- AutoCenter/Models/VehicleSpec.cs
+++ AutoCenter/Models/VehicleSpec.cs
@@ -8,16 +8,18 @@ namespace AutoCenter.Web.Models
         public int Id { get; set; }
         [Required]public int BrandId { get; set; }
         public Brand? Brand { get; set; } = null!;
+
         [Required]public int CarModelId { get; set; }
         public CarModel? CarModel { get; set; }
+
         [Required,Range(1930,2025)]public int Year { get; set; } 
         [Required, Range(0, 999)] public int Mileage { get; set; } 
 
-        //[Required]public VehicleSpecColor? Color { get; set; }
+        [Required]public VehicleSpecColor Color { get; set; }
         [Required,StringLength(32)]public string Vin { get; set; } = string.Empty;
-        [Required]public TransmissionType? Transmission { get; set; }
-        [Required]public FuelType? FuelType { get; set; }
-        [Required]public BodyType? BodyType { get; set; }
+        [Required]public TransmissionType Transmission { get; set; }
+        [Required]public FuelType FuelType { get; set; }
+        [Required]public BodyType BodyType { get; set; }
 
 
     }

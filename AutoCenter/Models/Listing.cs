34409d213ba14005cb152aa7d800ce670bd30800
Briberix <briberix.dev@gmail.com>
2025-09-15 20:39:08 +0200

    feat(listings): add OwnerId FK to Listing model

--- AutoCenter/Models/Listing.cs
+++ AutoCenter/Models/Listing.cs
@@ -5,17 +5,22 @@ namespace AutoCenter.Web.Models
     public class Listing 
     {
         public int Id { get; set; }
+
         [Required, StringLength(100, MinimumLength = 3)]
         public string Title { get; set; } = string.Empty;
+
         [StringLength(500, MinimumLength = 10)]
         public  string Description { get; set; } = string.Empty;
+
         [Required, Range(1,10_000_000)]
         public  decimal Price { get; set; }
+
         public bool IsActive { get; set; } = true;
 
-        //public required DateTime CreatedAt { get; set; } = DateTime.Now;
-        //public required DateTime UpdatedAt { get; set; } = DateTime.Now;
-        public int VehicleId { get; set; }
+        public string? OwnerId { get; set; }
+        public ApplicationUser Owner { get; set; } = null!;
+
+        public int VehicleSpecId { get; set; }
         public VehicleSpec Vehicle { get; set; } = null!;
     }
 
